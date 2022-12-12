24c670f3052666717bf402a45d719f6e94f4accc
Noé <lemor.noe@gmail.com>
2022-12-12 13:15:58 +0100

    HelperLocatorScreen ajout currentLocation et composant MapView

--- screens/HelpRequest/HelperLocatorScreen.js
+++ screens/HelpRequest/HelperLocatorScreen.js
@@ -1,20 +1,44 @@
-import { Button, StyleSheet, Text, View, KeyboardAvoidingView,  TextInput, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function HelperLocatorScreen({ navigation }) {
 
+  const dispatch = useDispatch();
+
+  //récupérer les données du store
+  const user = useSelector((state) => state.user.value);
+
+  const [currentPosition, setCurrentPosition] = useState(null);
+
+  //récupérer les données de géolocalisation
+  useEffect(() => {
+    (async () => {
+      //demander la permission
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      //si permission accordée
+      if (status === 'granted') {
+        //récupérer la localisation tous les 20m
+        Location.watchPositionAsync({ distanceInterval: 20 },
+          (location) => {
+            //transmettre les données des dernières coordonnées
+            setCurrentPosition(location.coords);
+          });
+      }
+    })();
+  }, []);
 
 
     return (
         <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-     <Text style={styles.title}> Page affichage Helper Map (step1) </Text>
-
-     
-     <TouchableOpacity style={styles.button} activeOpacity={0.9} onPress={() => navigation.navigate('HelperConfirmRequest')}>
-       <Text style={styles.textButton}>Poursuivre pour confirmer request</Text>
-     </TouchableOpacity>
-
-     
-
+          <Text style={styles.title}> Page affichage Helper Map (step1) </Text>
+          <MapView onLongPress={(e) => handleLongPress(e)} mapType="hybrid" style={styles.map}>
+        {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#E4513D" />}
+      </MapView>
+          <TouchableOpacity style={styles.button} activeOpacity={0.9} onPress={() => navigation.navigate('HelperConfirmRequest')}>
+            <Text style={styles.textButton}>Poursuivre pour confirmer request</Text>
+          </TouchableOpacity>
    </KeyboardAvoidingView>
    
    
