57b5a022ac448fcb4cc0d345ba7ac83e9669ae3f
Olivier <or.webtraining@gmail.com>
2022-12-13 11:19:06 +0100

    update en cours

--- screens/Sign/CguScreen.js
+++ screens/Sign/CguScreen.js
@@ -1,19 +1,49 @@
-import { Button, StyleSheet, Text, View, KeyboardAvoidingView,  TextInput, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, KeyboardAvoidingView,  Switch, TouchableOpacity } from 'react-native';
+import React from 'react';
 
 export default function CguScreen({ navigation }) {
+  const [isEnabled, setIsEnabled] = React.useState(false);
 
 
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-     <Text style={styles.title}>CGU acceptation Page </Text>
+      
+     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+     <View style={styles.container1}>
+     <Text style={styles.title}>CGU </Text>
+    
+     <Text>{isEnabled.toString()}</Text>
+     <Switch
+     style={styles.switch}
+        value={isEnabled}
+        onValueChange={(value) => setIsEnabled(value)}
+        trackColor={{ false: "#E6EBE0", true: "#5CA4A9" }}
+        thumbColor={isEnabled ? "#E6EBE0" : "#5CA4A9"}
+        ios_backgroundColor="#ff0000"
+        style={{ transform:[{ scaleX: 1.4 }, { scaleY: 1.4 }] }}
+      />
 
-     
-     <TouchableOpacity style={styles.button} activeOpacity={0.9} onPress={() => navigation.navigate('Signup')}>
-       <Text style={styles.textButton}>to the SignUp page</Text>
-     </TouchableOpacity>
+</View>
+
+<View style={styles.container2}>
+     <Text> 
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.Quis hendrerit dolor magna eget est lorem ipsum dolor. Quam lacus suspendisse faucibus interdum. Nisl suscipit adipiscing bibendum est. Ultrices dui sapien eget mi proin. Ut sem nulla pharetra diam. Morbi blandit cursus risus at ultrices mi tempus imperdiet nulla. Potenti nullam ac tortor vitae purus. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Tellus cras adipiscing enim eu.
+     </Text> 
+
+<Text style={styles.text2}>
+Veuillez accepter les conditions
+générales pour poursuivre
+votre inscription</Text>
+</View>
+<View style={styles.container2}>
+     <TouchableOpacity style={styles.button1} onPress={() => navigation.navigate('HelperDecline')}>
+       <Text style={styles.text1}>J'accepte</Text>
+      </TouchableOpacity>
+      </View> 
+
+    
    </KeyboardAvoidingView>
-   
    
    );
 }
@@ -25,12 +55,36 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    container1: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width:'90%',
+
+    },
+    container2: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width:'90%',
+          },
+
+    container3: {
+      flex:1  ,
+      backgroundColor: 'red',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width:'90%',
+      height:'50%',
+
+    },
     image: {
       width: '100%',
       height: '50%',
     },
     title: {
-      width: '80%',
+      width: '100%',
       fontSize: 38,
       fontWeight: '600',
     },
@@ -57,4 +111,30 @@ const styles = StyleSheet.create({
       fontWeight: '600',
       fontSize: 16,
     },
+    button1: {
+      marginTop: 10,
+    width: 176,
+    height: 48,
+    borderRadius: 10,
+    backgroundColor: "#33355C",
+    alignItems: "center",
+    justifyContent: "center",
+    },
+
+switch:{
+marginLeft:350,
+},
+    text1: {
+      color: "#FFFFFF",
+      // fontFamily: ,
+      fontWeight: "bold",
+      fontSize: 20,
+    },
+    text2: {
+      color: "#000000",
+      // fontFamily: ,
+      fontWeight: "bold",
+      fontSize: 20,
+    },
+
    });
