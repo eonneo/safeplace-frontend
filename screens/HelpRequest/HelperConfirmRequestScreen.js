a96401859082a2650edcb631e89c865a0b7da3ae
Noé <lemor.noe@gmail.com>
2022-12-15 11:30:38 +0100

    request confirmation screen

--- screens/HelpRequest/HelperConfirmRequestScreen.js
+++ screens/HelpRequest/HelperConfirmRequestScreen.js
@@ -1,8 +1,48 @@
-import { Button, StyleSheet, Text, View, KeyboardAvoidingView,  TextInput, TouchableOpacity } from 'react-native';
+import { Button, 
+  Image, 
+  StyleSheet, 
+  Text, 
+  View, 
+  KeyboardAvoidingView,  
+  TextInput, 
+  TouchableOpacity 
+} from 'react-native';
+
 import AppLoading  from 'expo-app-loading';
 import { useFonts } from '@use-expo/font';
 
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+//imports cards
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+const PlaceholderImage = require("../../assets/Vector.png");
+
 export default function HelperConfirmRequestScreen({ navigation }) {
+
+  const user = useSelector((state) => state.user.value);
+
+  const [currentPosition, setCurrentPosition] = useState(null);
+
+  //récupérer les données de géolocalisation
+  useEffect(() => {
+    (async () => {
+      //demander la permission
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      //si permission accordée
+      if (status === 'granted') {
+        //récupérer la localisation tous les 20m
+        Location.watchPositionAsync({ distanceInterval: 20 },
+          (location) => {
+            //transmettre les données des dernières coordonnées
+            setCurrentPosition(location.coords);
+          });
+      }
+    })();
+  }, []);
 
   const [isLoaded] = useFonts({
     'OpenSans': require("../../assets/OpenSans/OpenSans-Regular.ttf"),
@@ -11,18 +51,52 @@ export default function HelperConfirmRequestScreen({ navigation }) {
   if(!isLoaded) {
     return <AppLoading />
   }
-    return (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-     <Text style={styles.title}> Page affichage Confirmation demande d'aide (step2)  </Text>
-
-     
-     <TouchableOpacity style={styles.button} activeOpacity={0.9} onPress={() => navigation.navigate('ContactHelper')}>
-       <Text style={styles.textButton}>Naviguer vers la Contact Helper</Text>
-     </TouchableOpacity>
-   </KeyboardAvoidingView>
-   
-   
-   );
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <View style={styles.titlesContainer}> 
+        <Text style={styles.title}>X est le helper que tu as désigné pour t'aider</Text>
+        <Text style={styles.title}>Il arrive vers toi, tu peux suivre ses déplacements sur la carte</Text>
+      </View>
+      {currentPosition && <MapView mapType="standard" 
+        showsUserLocation={true} 
+        followsUserLocation={true} 
+        initialRegion={{
+          latitude: currentPosition.latitude,
+          longitude: currentPosition.longitude,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }} 
+        style={styles.map}>
+      </MapView>}
+      <TouchableOpacity style={styles.cardContent} onPress={() => navigation.navigate('ContactHelper')}>
+          <View style={styles.leftContent}>
+            <Image source={PlaceholderImage} style={styles.profilePic}></Image>
+            <View style={styles.middleContent}>
+              <Text style={styles.name}>Name</Text>
+              <Text style={styles.description}>Description</Text>
+              <Text style={styles.distance}>Distance</Text>
+            </View>
+          </View>
+          <View style={styles.rightContent}>
+            <View style={styles.isFavorite}>
+              <FontAwesome name="heart" size={20} color="#ec6e5b" />
+            </View>
+            <View style={styles.isConnected}>
+              <FontAwesome name="circle" size={20} color="#5CA4A9" />
+            </View>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.titlesContainer}>
+          <Text style={styles.title}>En attendant:</Text>
+          <TouchableOpacity>
+            <Text style={styles.link}>Quelques numéros utiles</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button3} onPress={() => navigation.navigate('ContactHelper')}>
+          <Text style={styles.text3}>Contacter X</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -30,38 +104,92 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#ffffff',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
+      paddingTop: 35,
     },
-    image: {
-      width: '100%',
-      height: '50%',
+    titlesContainer: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      width: "100%",
+      paddingBottom: 10,
+      paddingLeft: 15,
+      paddingRight: 15,
     },
     title: {
       width: '80%',
-      fontSize: 38,
-      fontWeight: '600',
-    },
-    input: {
-      width: '80%',
-      marginTop: 25,
-      borderBottomColor: '#EC6E5B',
-      borderBottomWidth: 1,
       fontSize: 20,
+      color: "#33355C",
+      fontWeight: '400',
     },
-    button: {
-      display: 'flex',
-      alignItems: 'center',
-      paddingTop: 8,
+    link: {
       width: '80%',
-      marginTop: 30,
-      backgroundColor: 'blue',
-      borderRadius: 10,
-      marginBottom: 80,
+      fontSize: 20,
+      color: "#5CA4A9",
+      fontWeight: '400',
     },
-    textButton: {
-      color: '#ffffff',
-      height: 30,
-      fontWeight: '600',
+    map: {
+      flex: 1,
+      width: "92%",
+    },
+    cardContent: {
+      width: "100%",
+      height: 70,
+      marginTop: 5,
+      marginBottom: 5,
+      backgroundColor: "white",
+      flexDirection: "row",
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    middleContent: {
+      marginLeft: 15,
+      color: '#33355C'
+    },
+    leftContent: {
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    rightContent: {
+      flexDirection: "row",
+      marginLeft: 140,
+      alignItems: 'center',
+      marginBottom: 20,
+    },
+    profilePic: {
+      width: 40,
+      height: 40,
+    },
+    name: {
+      fontSize: 24,
+      color: "#5CA4A9",
+    },
+    description: {
       fontSize: 16,
+      color: "#33355C",
+    },
+    distance: {
+      fontSize: 16,
+      color: "#33355C",
+    },
+    isFavorite: {
+      marginRight: 20,
+    },
+    button3: {
+      marginTop: 5,
+      marginBottom: 15,
+      width: 213,
+      height: 48,
+      borderRadius: 10,
+      backgroundColor: "#33355C",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    text3: {
+      color: "#FFFFFF",
+      fontFamily: 'OpenSans',
+      fontWeight: "bold",
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: 20,
     },
    });
