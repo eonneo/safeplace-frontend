4878b9eca0a793c6cd879bf870b7afbe4741b5a4
Pierre CHAPELOT <pierre.chapelot@gmail.com>
2022-12-20 17:47:01 +0100

    style of ressources, police screens

--- screens/TabNavigator/PoliceScreen.js
+++ screens/TabNavigator/PoliceScreen.js
@@ -1,124 +1,265 @@
-import { Platform, StyleSheet, Text, View, SafeAreaView,  Linking, TouchableOpacity } from 'react-native';
-import { useEffect, useState } from 'react';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
-import { useFonts } from '@use-expo/font';
-import * as SMS from 'expo-sms';
-import { useSelector, } from 'react-redux';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "@use-expo/font";
+import * as SMS from "expo-sms";
+import { useSelector } from "react-redux";
 
 export default function PoliceScreen({ navigation }) {
   const [isAvailable, setIsAvailable] = useState(false);
   const user = useSelector((state) => state.user.value);
-  const PlaceholderImage = require("../../assets/Vector.png");
+  const PlaceholderImage = require("../../assets/Emergencypic.png");
 
-// pour utilisation installer d'abord => expo install expo-sms
- 
+  // pour utilisation installer d'abord => expo install expo-sms
 
-  useEffect( () => {
+  useEffect(() => {
     async function checkAvailability() {
-      
       const isSmsAvailable = await SMS.isAvailableAsync();
       setIsAvailable(isSmsAvailable);
     }
     checkAvailability();
   }, []);
-  
 
-  // Enregistrer ci-dessous le numéro de téléphone + Message 
+  // Enregistrer ci-dessous le numéro de téléphone + Message
   const sendSms = async () => {
-  const {result} = await SMS.sendSMSAsync (
-  ['0665331020'], `Hello ${user.prenom} need your Help, please find below more details`)
-  ;
-  
-  console.log(result)
-  
-  
+    const { result } = await SMS.sendSMSAsync(
+      ["0665331020"],
+      `Hello ${user.prenom} need your Help, please find below more details`
+    );
+    console.log(result);
   };
 
- const makeCall = () => {
+  const makeCall = () => {
+    let phoneNumber = "";
 
-    let phoneNumber = '';
-
-    if (Platform.OS === 'android') {
-      phoneNumber = 'tel:${0666505366}';
+    if (Platform.OS === "android") {
+      phoneNumber = "tel:${0666505366}";
     } else {
-      phoneNumber = 'telprompt:${0666505366}';
+      phoneNumber = "telprompt:${0666505366}";
     }
 
     Linking.openURL(phoneNumber);
   };
 
-
+  // Font
   const [isLoaded] = useFonts({
-    'OpenSans': require("../../assets/OpenSans/OpenSans-Regular.ttf"),
-    'Raleway': require('../../assets/Raleway/static/Raleway-Regular.ttf')
-    });
-  if(!isLoaded) {
-    return <View />
+    OpenSans: require("../../assets/OpenSans/OpenSans-Regular.ttf"),
+    Raleway: require("../../assets/Raleway/static/Raleway-Regular.ttf"),
+  });
+  if (!isLoaded) {
+    return <View />;
   }
-    return (
-        <SafeAreaView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-     <Text style={styles.title}> Emergency page  </Text>
+  return (
+    <ScrollView>
+      <SafeAreaView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TouchableOpacity
+          style={styles.header}
+          onPress={() => navigation.navigate("Profil")}
+        >
+          <Text style={styles.nameText}>{user.prenom}</Text>
+          <Image
+            source={{ uri: `${user.avatarUri}` }}
+            style={styles.profilePic}
+          ></Image>
+        </TouchableOpacity>
+        <Text style={styles.title}>En cas d'urgence</Text>
 
-     
-     <TouchableOpacity style={styles.button} activeOpacity={0.9} onPress={() => sendSms()}>
-       <Text style={styles.textButton}>Envoyer un message d'urgence</Text>
-     </TouchableOpacity>
+        <View style={styles.lineStyle} />
+        <View style={styles.optionContainer}>
+          <TouchableOpacity
+            style={styles.disconnectButton}
+            activeOpacity={0.9}
+            onPress={() => sendSms()}
+          >
+            <Text style={styles.disconnectText}>Envoyer SMS Police</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.disconnectButton}
+            activeOpacity={0.9}
+            onPress={() => navigation.navigate("EmergencyNumbScreen")}
+          >
+            <Text style={styles.disconnectText}>Numéros d'urgence</Text>
+          </TouchableOpacity>
+        </View>
+       
+        <Image source={PlaceholderImage} style={styles.homePic}></Image>
 
-     <TouchableOpacity style={styles.button} activeOpacity={0.9} onPress={() => navigation.navigate('EmergencyNumbScreen')}>
-       <Text style={styles.textButton}>Test affichage numéros d'urgences</Text>
-     </TouchableOpacity>
-   
-
-
-
-   </SafeAreaView>
-   
-   
-   );
+        <View style={styles.lineStyle} />
+      </SafeAreaView>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    image: {
-      width: '100%',
-      height: '50%',
-    },
-    title: {
-      width: '80%',
-      fontSize: 38,
-      fontWeight: '600',
-      fontFamily:'OpenSans',
-      textAlign:'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+  },
 
-    input: {
-      width: '80%',
-      marginTop: 25,
-      borderBottomColor: '#EC6E5B',
-      borderBottomWidth: 1,
-      fontSize: 20,
-    },
-    
-    button: {
-      marginTop: 10,
-      width: 300,
-      height: 48,
-      borderRadius: 10,
-      backgroundColor: "#33355C",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    textButton: {
-      color: '#ffffff',
-      fontFamily:'Raleway',
-      height: 30,
-      fontWeight: '600',
-      fontSize: 16,
-    },
-   });
+  header: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  nameText: {
+    width: "80%",
+    fontSize: 24,
+    color: "#33355C",
+    fontWeight: "400",
+    fontFamily: "Raleway",
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "#33355C",
+    fontFamily: "OpenSans",
+    paddingTop: 20,
+    paddingBottom: 5,
+  },
+  lineStyle: {
+    borderWidth: 0.5,
+    width: "80%",
+    borderColor: "#33355C",
+    margin: 10,
+    alignSelf: "center",
+    marginTop: 5,
+    marginBottom: 15,
+  },
+
+
+  textContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    justifyContent: "flex-start",
+    fontFamily: "Raleway",
+    color: "#5CA4A9",
+    marginBottom: 10,
+  },
+  smallText: {
+    fontSize: 14,
+    marginBottom: 20,
+    color: "#33355C",
+    width: "100%",
+    fontFamily: "OpenSans",
+  },
+  disconnectButton: {
+    marginTop: 20,
+    marginBottom: 20,
+    width: 176,
+    height: 48,
+    borderRadius: 10,
+    backgroundColor: "#E4513D",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  disconnectText: {
+    color: "#FFFFFF",
+    fontFamily: "OpenSans",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  homePic: {
+    flex: 1,
+    resizeMode: 'contain'
+  },
+});
+
+//   style={styles.container}
+//   behavior={Platform.OS === "ios" ? "padding" : "height"}
+// >
+//   <Text style={styles.title}> Emergency page </Text>
+
+//   <TouchableOpacity
+//     style={styles.button}
+//     activeOpacity={0.9}
+//     onPress={() => sendSms()}
+//   >
+//     <Text style={styles.textButton}>Envoyer un message d'urgence</Text>
+//   </TouchableOpacity>
+
+//   <TouchableOpacity
+//     style={styles.button}
+//     activeOpacity={0.9}
+//     onPress={() => navigation.navigate("EmergencyNumbScreen")}
+//   >
+//     <Text style={styles.textButton}>Test affichage numéros d'urgences</Text>
+//   </TouchableOpacity>
+// </SafeAreaView>
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#ffffff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   image: {
+//     width: "100%",
+//     height: "50%",
+//   },
+//   title: {
+//     width: "80%",
+//     fontSize: 38,
+//     fontWeight: "600",
+//     fontFamily: "OpenSans",
+//     textAlign: "center",
+//   },
+
+//   input: {
+//     width: "80%",
+//     marginTop: 25,
+//     borderBottomColor: "#EC6E5B",
+//     borderBottomWidth: 1,
+//     fontSize: 20,
+//   },
+
+//   button: {
+//     marginTop: 10,
+//     width: 300,
+//     height: 48,
+//     borderRadius: 10,
+//     backgroundColor: "#33355C",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   textButton: {
+//     color: "#ffffff",
+//     fontFamily: "Raleway",
+//     height: 30,
+//     fontWeight: "600",
+//     fontSize: 16,
+//   },
+// });
--- screens/TabNavigator/RessourcesScreen.js
+++ screens/TabNavigator/RessourcesScreen.js
@@ -1,104 +1,179 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView,   TouchableOpacity, ScrollView } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import {
+  StyleSheet,
+  Text,
+  View,
 
-import { useFonts } from '@use-expo/font';
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector } from "react-redux";
+import { useFonts } from "@use-expo/font";
 
 export default function RessourcesScreen({ navigation }) {
+  const user = useSelector((state) => state.user.value);
 
+  // Font
   const [isLoaded] = useFonts({
-    'OpenSans': require("../../assets/OpenSans/OpenSans-Regular.ttf"),
-    'Raleway': require('../../assets/Raleway/static/Raleway-Regular.ttf')
-    });
-  if(!isLoaded) {
-    return <View />
+    OpenSans: require("../../assets/OpenSans/OpenSans-Regular.ttf"),
+    Raleway: require("../../assets/Raleway/static/Raleway-Regular.ttf"),
+  });
+  if (!isLoaded) {
+    return <View />;
   }
-    return (
-        <SafeAreaView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-<View style={styles.container1}>
+  return (
+    <ScrollView>
+      <SafeAreaView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TouchableOpacity
+          style={styles.header}
+          onPress={() => navigation.navigate("Profil")}
+        >
+          <Text style={styles.nameText}>{user.prenom}</Text>
+          <Image
+            source={{ uri: `${user.avatarUri}` }}
+            style={styles.profilePic}
+          ></Image>
+        </TouchableOpacity>
+        <Text style={styles.title}>Helper's Club </Text>
 
-     <Text style={styles.title}> Helper's Club  </Text>
-     <Text> Et voilou, les supers conseils !!!   </Text>
+        <View style={styles.lineStyle} />
+        <View style={styles.optionContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.subtitle}>Que faire en cas d'urgence ?</Text>
+            <Text style={styles.smallText}>
+              Alerter la police et la gendarmerie par téléphone Si la personne
+              avec qui vous vivez en couple vous fait subir des actes de
+              violences conjugales ou menace de le faire, vous pouvez alerter la
+              police ou la gendarmerie.
+            </Text>
+          </View>
+        </View>
+        <View style={styles.optionContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.subtitle}>
+              Comment aider une victime de violences conjugales ?
+            </Text>
+            <Text style={styles.smallText}>
+              Vous n'avez pas besoin de beaucoup parler pour être présent pour
+              quelqu'un. Une victime a principalement besoin de confier combien
+              ce qui se passe dans son couple est difficile, le poids de son
+              isolement, la culpabilité qu'elle ressent. Elle a besoin de rompre
+              le secret. Écouter sans jugement est un acte de bienveillance et
+              c'est déjà beaucoup.
+            </Text>
+          </View>
+        </View>
+        <View style={styles.optionContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.subtitle}>
+              Demandez-lui ce que vous pouvez faire pour l'aider
+            </Text>
+            <Text style={styles.smallText}>
+              La violence est liée au pouvoir et au contrôle. Il est primordial
+              que les victimes retrouvent leur libre arbitre, décident de ce
+              qu'elles souhaitent faire, en toute indépendance. Il peut être
+              désastreux de pousser une personne à agir alors qu'elle ne s'en
+              sent pas prête (porter plainte, partir du foyer…) Laissez la
+              personne suivre son propre rythme. Ne remplacez pas la dépendance
+              qu'elle avait à l'homme violent par une dépendance à vous, son
+              ami(e), sa collègue, un membre de sa famille…
+            </Text>
+          </View>
+        </View>
 
-
-     </View>
-     <View style={styles.container2}>
-     <Text style={styles.smalltitle}>Que faire en cas d'urgence ?  </Text>
-     <Text style={styles.paragraphe}>
-     Alerter la police et la gendarmerie par téléphone
-Si la personne avec qui vous vivez en couple vous fait subir des actes de violences conjugales ou menace de le faire, vous pouvez alerter la police ou la gendarmerie.
-     </Text>
-
-     <Text style={styles.smalltitle}>Comment aider une victime de violences conjugales ?  </Text>
-     <Text style={styles.paragraphe}>
-     Vous n’avez pas besoin de beaucoup parler pour être présent pour quelqu’un. Une victime a principalement besoin de confier combien ce qui se passe dans son couple est difficile, le poids de son isolement, la culpabilité qu’elle ressent. Elle a besoin de rompre le secret. Écouter sans jugement est un acte de bienveillance et c’est déjà beaucoup.
-     </Text>
-
-
-     <Text style={styles.smalltitle}>Demandez-lui ce que vous pouvez faire pour l’aider  </Text>
-     <Text style={styles.paragraphe}>
-     La violence est liée au pouvoir et au contrôle. Il est primordial que les victimes retrouvent leur libre arbitre, décident de ce qu’elles souhaitent faire, en toute indépendance. Il peut être désastreux de pousser une personne à agir alors qu’elle ne s’en sent pas prête (porter plainte, partir du foyer…)
-Laissez la personne suivre son propre rythme. Ne remplacez pas la dépendance qu’elle avait à l’homme violent par une dépendance à vous, son ami(e), sa collègue, un membre de sa famille…
-     </Text>
-
-    
-
-     </View>
-
-   </SafeAreaView>
-   
-   
-   );
+        <View style={styles.lineStyle} />
+      </SafeAreaView>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+  },
 
-    container1: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width:'100%',
-    },
+  header: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  nameText: {
+    width: "80%",
+    fontSize: 24,
+    color: "#33355C",
+    fontWeight: "400",
+    fontFamily: "Raleway",
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "#33355C",
+    fontFamily: "OpenSans",
+    paddingTop: 20,
+    paddingBottom: 5,
+  },
+  lineStyle: {
+    borderWidth: 0.5,
+    width: "80%",
+    borderColor: "#33355C",
+    margin: 10,
+    alignSelf: "center",
+    marginTop: 5,
+    marginBottom: 15,
+  },
 
-    container2: {
-      flex: 3,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width:'100%',
-
-    },
-    
-    title: {
-      fontSize: 38,
-      fontWeight: '600',
-      textAlign:'center',
-      color:'#5CA4A9',
-    },
-
-    smalltitle: {
-      marginBottom:5,
-      width: '80%',
-      fontSize: 24,
-      fontWeight: '600',
-      textAlign:'left',
-      color:'#5CA4A9',
-
-    },
-
-    paragraphe: {
-      marginBottom:10,
-
-      width: '80%',
-      fontSize: 12,
-      fontWeight: '300',
-      textAlign:'left',
-    },
-
-   });
+  // scrollView: {
+  //   flex: 1,
+  //   width: "100%",
+  // },
+  optionContainer: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  textContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    justifyContent: "flex-start",
+    fontFamily: "Raleway",
+    color: "#5CA4A9",
+    marginBottom: 10,
+  },
+  smallText: {
+    fontSize: 14,
+    marginBottom: 20,
+    color: "#33355C",
+    width: "100%",
+    fontFamily: "OpenSans",
+  },
+ 
+});
